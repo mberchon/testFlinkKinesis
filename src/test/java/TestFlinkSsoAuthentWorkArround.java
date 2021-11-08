5d8dca7059f365e803a21737b9bde476a9352339
mathieuberchon <mberchon@gmail.com>
2021-11-08 15:26:15 +0100

    Comparison between flink and kinesis sdk

--- src/test/java/TestFlinkSsoAuthentWorkArround.java
+++ src/test/java/TestFlinkSsoAuthentWorkArround.java
@@ -1,24 +1,24 @@
+import com.amazonaws.services.securitytoken.model.GetSessionTokenResult;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.junit.Test;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.kinesis.KinesisClient;
 import software.amazon.kinesis.connectors.flink.FlinkKinesisConsumer;
 import software.amazon.kinesis.connectors.flink.config.AWSConfigConstants;
 import software.amazon.kinesis.connectors.flink.config.ConsumerConfigConstants;
 
 import java.util.Properties;
 
-public class TestFlinkSsoAuthent {
+public class TestFlinkSsoAuthentWorkArround {
 
     @Test
-    public void testFlink() throws Exception {
+    public void test() throws Exception {
 
         StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
         see.setParallelism(1);
+
+        var provider = DefaultCredentialsProvider.create();
 
         Properties kinesisConsumerConfig = new Properties();
         kinesisConsumerConfig.setProperty(ConsumerConfigConstants.AWS_REGION, "us-east-1");
@@ -33,17 +33,6 @@ public class TestFlinkSsoAuthent {
         kinesis.print();
 
         see.execute();
-
-    }
-
-    @Test
-    public void testKdsSdk() {
-        var client = KinesisClient.builder()
-                .region(Region.US_EAST_1)
-                .credentialsProvider(DefaultCredentialsProvider.create())
-                .build();
-
-        System.out.println(client.listStreams());
 
     }
 
